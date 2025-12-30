dc68c7d29ef878cc31be98704e60979bfb86f142
David Stadelmann <david.stadelmann@scs.ch>
2025-12-30 22:55:24 +0100

    Add tests for cpm

---
+++ tests/unit/test_cpm.py
@@ -0,0 +1,53 @@
+from aoa.model.activity import Activity
+from aoa.model.cpm import calculate_earliest_start, calculate_latest_finish
+from aoa.model.network import Network
+
+
+def test_earliest_start_calculation() -> None:
+    activities = [
+        Activity(
+            id=1,
+            effort=5,
+        ),
+        Activity(
+            id=2,
+            effort=3,
+            predecessors=set([1]),
+        ),
+    ]
+
+    network = Network(activities)
+    calculate_earliest_start(network)
+    assert activities[0].earliest_start == 0
+    assert activities[1].earliest_start == activities[0].effort
+
+
+def test_latest_finish_calculation() -> None:
+    activities = [
+        Activity(
+            id=1,
+            effort=10,
+        ),
+        Activity(
+            id=2,
+            effort=1,
+        ),
+        Activity(
+            id=3,
+            effort=1,
+            predecessors={2},
+        ),
+        Activity(
+            id=4,
+            effort=1,
+            predecessors={3},
+        ),
+        Activity(
+            id=5,
+            effort=1,
+            predecessors={2},
+        ),
+    ]
+
+    network = Network(activities)
+    # calculate_latest_finish(network)
